4db1aa8b31715c62e6820b89029506ed8b8153e2
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2020-05-27 23:31:30 -0300

    Criacao do projeto

---
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.gradecurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCurricularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.gradecurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

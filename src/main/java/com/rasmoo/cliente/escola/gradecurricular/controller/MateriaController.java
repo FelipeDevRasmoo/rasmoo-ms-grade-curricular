6acae0bc87bf3a0cc39ec74d0255dd55a64fddc8
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2020-06-07 16:52:17 -0300

    Resposta aula 5

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/MateriaController.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/MateriaController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,48 @@ public class MateriaController {
 			return ResponseEntity.status(HttpStatus.OK).body(true);
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.OK).body(false);
+		}
+	}
+	
+	/*
+	 * METODO DELETE
+	 * 
+	 */
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> excluirMateria(@PathVariable Long id) {
+		try {
+			this.materiaRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.OK).body(true);
+			
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.OK).body(true);
+		}
+	}
+	
+	/*
+	 * METODO PUT
+	 * 
+	 */
+	@PutMapping
+	public ResponseEntity<Boolean> atualizarMateria(@RequestBody MateriaEntity materia) {
+		try {
+			//buscamos pela materia que gostaríamos de atualizar
+			MateriaEntity materiaEntityAtualizada = this.materiaRepository.findById(materia.getId()).get();
+			
+			//atualizamos todos os valores
+			materiaEntityAtualizada.setNome(materia.getNome());
+			materiaEntityAtualizada.setCodigo(materia.getCodigo());
+			materiaEntityAtualizada.setHoras(materia.getHoras());
+			materiaEntityAtualizada.setNome(materia.getNome());
+			materiaEntityAtualizada.setFrequencia(materia.getFrequencia());
+			
+			//salvamos as alteracoes
+			this.materiaRepository.save(materiaEntityAtualizada);
+			
+			return ResponseEntity.status(HttpStatus.OK).body(true);
+			
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.OK).body(true);
 		}
 	}
 

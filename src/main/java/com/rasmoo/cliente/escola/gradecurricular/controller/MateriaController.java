22855aba1298055be1034e0e5d0b480caf502654
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2020-06-07 16:03:34 -0300

    Implementacao da entidade MateriaEntity

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/MateriaController.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/MateriaController.java
@@ -1,18 +1,45 @@
 package com.rasmoo.cliente.escola.gradecurricular.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rasmoo.cliente.escola.gradecurricular.entity.MateriaEntity;
+import com.rasmoo.cliente.escola.gradecurricular.repository.IMateriaRepository;
 
 @RestController
 @RequestMapping("/materia")
 public class MateriaController {
-	
-	@GetMapping("/")
-	public ResponseEntity<String> helloWorldRest(){
-		return ResponseEntity.status(HttpStatus.OK).body("Olá mundo REST");
+
+	@Autowired
+	private IMateriaRepository materiaRepository;
+
+	@GetMapping
+	public ResponseEntity<List<MateriaEntity>> listarMaterias() {
+		return ResponseEntity.status(HttpStatus.OK).body(this.materiaRepository.findAll());
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<MateriaEntity> consultaMateria(@PathVariable Long id) {
+		return ResponseEntity.status(HttpStatus.OK).body(this.materiaRepository.findById(id).get());
+	}
+
+	@PostMapping
+	public ResponseEntity<Boolean> cadastrarMateria(@RequestBody MateriaEntity materia) {
+		try {
+			this.materiaRepository.save(materia);
+			return ResponseEntity.status(HttpStatus.OK).body(true);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.OK).body(false);
+		}
+	}
+
 }

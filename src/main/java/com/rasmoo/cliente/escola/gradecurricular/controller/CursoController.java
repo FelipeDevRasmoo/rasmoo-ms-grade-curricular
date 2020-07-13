acd7cd5500137633112eb0ad1577bde9c7e0a4d4
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2020-07-13 18:11:48 -0300

    Configuracao banco de dados

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/CursoController.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/CursoController.java
@@ -38,9 +38,9 @@ public class CursoController {
 		Response<Boolean> response = new Response<>();
 
 		response.setData(cursoService.cadastrar(curso));
-		response.setStatusCode(HttpStatus.OK.value());
+		response.setStatusCode(HttpStatus.CREATED.value());
 
-		return ResponseEntity.status(HttpStatus.OK).body(response);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 
 	}
 

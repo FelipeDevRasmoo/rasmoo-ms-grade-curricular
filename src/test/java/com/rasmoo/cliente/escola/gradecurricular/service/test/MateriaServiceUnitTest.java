9e8d7aba60d0a9c1f2e749ba4d1c3e9e260f2319
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2020-07-19 03:10:41 -0300

    Testes uni na MateriaService cenario Sucesso

--- src/test/java/com/rasmoo/cliente/escola/gradecurricular/service/test/MateriaServiceUnitTest.java
+++ src/test/java/com/rasmoo/cliente/escola/gradecurricular/service/test/MateriaServiceUnitTest.java
@@ -38,12 +38,14 @@ public class MateriaServiceUnitTest {
 
 	@BeforeAll
 	public static void init() {
+
 		materiaEntity = new MateriaEntity();
 		materiaEntity.setId(1L);
 		materiaEntity.setCodigo("ILP");
-		materiaEntity.setFrequencia(2);
-		materiaEntity.setHoras(34);
-		materiaEntity.setNome("INTRODUCAO A LINGUAGEM DE PROGRAMCAO");
+		materiaEntity.setFrequencia(1);
+		materiaEntity.setHoras(64);
+		materiaEntity.setNome("INTRODUCAO A LINGUAGEM DE PROGRAMACAO");
+
 	}
 
 	/*
@@ -53,23 +55,10 @@ public class MateriaServiceUnitTest {
 	 */
 
 	@Test
-	public void testConsultarSuccesso() {
-		Mockito.when(this.materiaRepository.findById(1L)).thenReturn(Optional.of(materiaEntity));
-
-		MateriaDto materiaDto = this.materiaService.consultar(1L);
-
-		assertNotNull(materiaDto);
-		assertEquals("ILP", materiaDto.getCodigo());
-		assertEquals(1, materiaDto.getId());
-		assertEquals(2, materiaDto.getFrequencia());
-
-		Mockito.verify(this.materiaRepository, times(1)).findById(1L);
-	}
-
-	@Test
-	public void testListarSuccesso() {
+	public void testListarSucesso() {
 		List<MateriaEntity> listMateria = new ArrayList<>();
 		listMateria.add(materiaEntity);
+
 		Mockito.when(this.materiaRepository.findAll()).thenReturn(listMateria);
 
 		List<MateriaDto> listMateriaDto = this.materiaService.listar();
@@ -81,12 +70,14 @@ public class MateriaServiceUnitTest {
 		assertEquals(1, listMateriaDto.size());
 
 		Mockito.verify(this.materiaRepository, times(1)).findAll();
+
 	}
 
 	@Test
-	public void testListarPorHorarioMinimoSuccesso() {
+	public void testListarPorHorarioMinimoSucesso() {
 		List<MateriaEntity> listMateria = new ArrayList<>();
 		listMateria.add(materiaEntity);
+
 		Mockito.when(this.materiaRepository.findByHoraMinima(64)).thenReturn(listMateria);
 
 		List<MateriaDto> listMateriaDto = this.materiaService.listarPorHorarioMinimo(64);
@@ -97,81 +88,102 @@ public class MateriaServiceUnitTest {
 		assertEquals(1, listMateriaDto.size());
 
 		Mockito.verify(this.materiaRepository, times(1)).findByHoraMinima(64);
+
 	}
 
 	@Test
-	public void testListarPorFrequenciaSuccesso() {
+	public void testListarPorFrequenciaSucesso() {
 		List<MateriaEntity> listMateria = new ArrayList<>();
 		listMateria.add(materiaEntity);
-		Mockito.when(this.materiaRepository.findByFrequencia(2)).thenReturn(listMateria);
 
-		List<MateriaDto> listMateriaDto = this.materiaService.listarPorFrequencia(2);
+		Mockito.when(this.materiaRepository.findByFrequencia(1)).thenReturn(listMateria);
+
+		List<MateriaDto> listMateriaDto = this.materiaService.listarPorFrequencia(1);
 
 		assertNotNull(listMateriaDto);
 		assertEquals("ILP", listMateriaDto.get(0).getCodigo());
 		assertEquals(1, listMateriaDto.get(0).getId());
 		assertEquals(1, listMateriaDto.size());
 
-		Mockito.verify(this.materiaRepository, times(1)).findByFrequencia(2);
+		Mockito.verify(this.materiaRepository, times(1)).findByFrequencia(1);
+
 	}
 
 	@Test
-	public void testCadastrarSuccesso() {
+	public void testConsultarSucesso() {
+		Mockito.when(this.materiaRepository.findById(1L)).thenReturn(Optional.of(materiaEntity));
+		MateriaDto materiaDto = this.materiaService.consultar(1L);
+
+		assertNotNull(materiaDto);
+		assertEquals("ILP", materiaDto.getCodigo());
+		assertEquals(1, materiaDto.getId());
+		assertEquals(1, materiaDto.getFrequencia());
+
+		Mockito.verify(this.materiaRepository, times(1)).findById(1L);
+	}
+
+	@Test
+	public void testCadastrarSucesso() {
 
 		MateriaDto materiaDto = new MateriaDto();
 		materiaDto.setCodigo("ILP");
-		materiaDto.setFrequencia(2);
-		materiaDto.setHoras(34);
-		materiaDto.setNome("INTRODUCAO A LINGUAGEM DE PROGRAMCAO");
+		materiaDto.setFrequencia(1);
+		materiaDto.setHoras(64);
+		materiaDto.setNome("INTRODUCAO A LINGUAGEM DE PROGRAMACAO");
 
 		materiaEntity.setId(null);
 
 		Mockito.when(this.materiaRepository.findByCodigo("ILP")).thenReturn(null);
 		Mockito.when(this.materiaRepository.save(materiaEntity)).thenReturn(materiaEntity);
 
-		Boolean sucessoCadastro = this.materiaService.cadastrar(materiaDto);
+		Boolean sucesso = this.materiaService.cadastrar(materiaDto);
 
-		assertTrue(sucessoCadastro);
+		assertTrue(sucesso);
 
-		Mockito.verify(this.materiaRepository, times(1)).save(materiaEntity);
 		Mockito.verify(this.materiaRepository, times(1)).findByCodigo("ILP");
+		Mockito.verify(this.materiaRepository, times(1)).save(materiaEntity);
 
 		materiaEntity.setId(1L);
-	}
 
+	}
+	
 	@Test
-	public void testAtualizarSuccesso() {
+	public void testAtualizarSucesso() {
 
 		MateriaDto materiaDto = new MateriaDto();
 		materiaDto.setId(1L);
 		materiaDto.setCodigo("ILP");
-		materiaDto.setFrequencia(2);
-		materiaDto.setHoras(34);
-		materiaDto.setNome("INTRODUCAO A LINGUAGEM DE PROGRAMCAO");
+		materiaDto.setFrequencia(1);
+		materiaDto.setHoras(64);
+		materiaDto.setNome("INTRODUCAO A LINGUAGEM DE PROGRAMACAO");
+
 
 		Mockito.when(this.materiaRepository.findById(1L)).thenReturn(Optional.of(materiaEntity));
 		Mockito.when(this.materiaRepository.save(materiaEntity)).thenReturn(materiaEntity);
 
-		Boolean sucessoAtualizacao = this.materiaService.atualizar(materiaDto);
+		Boolean sucesso = this.materiaService.atualizar(materiaDto);
 
-		assertTrue(sucessoAtualizacao);
+		assertTrue(sucesso);
 
+		Mockito.verify(this.materiaRepository, times(0)).findByCodigo("ILP");
+		Mockito.verify(this.materiaRepository, times(1)).findById(1L);
 		Mockito.verify(this.materiaRepository, times(1)).save(materiaEntity);
-		Mockito.verify(this.materiaRepository, times(1)).findById(1L);
+
+
 	}
-
+	
 	@Test
-	public void testExcluirSuccesso() {
-
+	public void testExcluirSucesso() {
+		
 		Mockito.when(this.materiaRepository.findById(1L)).thenReturn(Optional.of(materiaEntity));
+		Boolean sucesso = this.materiaService.excluir(1L);
 
-		Boolean sucessoCadastro = this.materiaService.excluir(1L);
+		assertTrue(sucesso);
 
-		assertTrue(sucessoCadastro);
-
-		Mockito.verify(this.materiaRepository, times(1)).deleteById(1L);
+		Mockito.verify(this.materiaRepository, times(0)).findByCodigo("ILP");
 		Mockito.verify(this.materiaRepository, times(1)).findById(1L);
-
+		Mockito.verify(this.materiaRepository, times(1)).deleteById(1L);
+		Mockito.verify(this.materiaRepository, times(0)).save(materiaEntity);
 	}
 
 	/*

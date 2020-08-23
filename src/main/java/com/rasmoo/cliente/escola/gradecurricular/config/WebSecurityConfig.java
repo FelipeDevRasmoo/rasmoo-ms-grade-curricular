fd2d3a57c3068e86da11d6b9516aa0683f2fd360
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2020-08-23 18:04:58 -0300

    Camada de autenticacao

---
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.rasmoo.cliente.escola.gradecurricular.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	public void configuracaoGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		PasswordEncoder pass = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("rasmoo").password(pass.encode("msgradecurricular")).roles("ADMIN");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable().authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
+	}
+	
+}

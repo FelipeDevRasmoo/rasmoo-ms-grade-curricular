e2bb924b3e843feaed0324c4288c58848c9516ad
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2020-08-23 00:47:05 -0300

    Resp Doc MateriaController

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/config/SwaggerConfig.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/config/SwaggerConfig.java
@@ -22,18 +22,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport{
-	
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
 	public static final String CURSO = "Curso";
-	
+	public static final String MATERIA = "Matéria";
+
 	@Bean
 	public Docket gradeCurricularApi() {
 		return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
 				.apis(RequestHandlerSelectors.basePackage("com.rasmoo.cliente.escola.gradecurricular")).build()
-				.apiInfo(this.metaData())
-				.tags(new Tag(CURSO, "Operações referentes a manipulação da entidade Curso."));
+				.apiInfo(this.metaData()).tags(new Tag(CURSO, "Operações referentes a manipulação da entidade Curso."))
+				.tags(new Tag(MATERIA, "Operações referentes a manipulação da entidade Materia."));
 	}
-	
+
 	@Bean
 	public LinkDiscoverers discoverers() {
 		List<LinkDiscoverer> listPlugins = new ArrayList<>();
@@ -43,10 +44,10 @@ public class SwaggerConfig extends WebMvcConfigurationSupport{
 
 	private ApiInfo metaData() {
 		return new ApiInfoBuilder().title("GRADE CURRICULAR")
-				.description("Api responsável pela manutenção da grande curricular da RASMOO SCHOOL")
-				.version("1.0.0").license("").build();
+				.description("Api responsável pela manutenção da grande curricular da RASMOO SCHOOL").version("1.0.0")
+				.license("").build();
 	}
-	
+
 	@Override
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:META-INF/resources/");
